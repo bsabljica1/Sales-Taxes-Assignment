12abf9972168ca38568d676e80d0420878a4147f
bsabljica1 <bsabljica1@etf.unsa.ba>
2021-12-02 17:08:21 +0100

    created a class for storing every item added to the shopping cart and a basic console application for entering items

--- assignment/src/main/java/com/sales_taxes/App.java
+++ assignment/src/main/java/com/sales_taxes/App.java
@@ -1,10 +1,141 @@
 package com.sales_taxes;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+import com.sales_taxes.model.Item;
 
 public final class App {
-    private App() {
-    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        List<Item> items = new ArrayList<Item>();
+        int itemCount = 1;
+
+
+        while (true) {
+
+        String name;
+        Double price;
+        int quantity;
+        Boolean imported;
+        int type;
+
+        Scanner sc= new Scanner(System.in);
+
+        System.out.println("\nAdd items to your shopping cart!\n");
+        System.out.println("Add item number " + itemCount + "\n");
+        
+        System.out.println("Insert item name: \n");
+        name = sc.nextLine();
+        
+        System.out.println("\nInsert item price: \n");
+
+        while (true) {
+
+        String str = sc.nextLine();
+
+        if( str.matches("^\\d+\\.\\d+") || str.matches("^\\d+")) {
+            price = Double.parseDouble(str);
+            price = Math.round(price*100.0)/100.0;
+            break;
+        }
+		else
+			System.out.println("\nInvalid entry, insert a valid item price:\n");
+        }
+
+        System.out.println("\nInsert item quantity: \n");
+
+        while (true) {
+
+            try {
+
+            int nmb = sc.nextInt();
+
+            quantity = nmb;
+            break;
+
+            }
+            catch (InputMismatchException n) {
+
+                System.err.println("\nInvalid entry, insert a valid item quantity:\n");
+                sc.nextLine();
+            }
+
+        }
+
+        System.out.println("\nIs the item imported : (Y/N) [any other key except y/Y is considered as N]\n");
+        sc.nextLine();
+
+        String str = sc.nextLine();
+
+        if (str.matches("y") || str.matches("Y"))
+            imported = true;
+        else
+            imported = false;
+
+        System.out.println("\nSelect the type of item (select by typing one of the numbers next to the options)\n1. Book\n2. Food\n3. Medical\n4. Other\n");
+
+        while (true) {
+
+            boolean isNumber = false;
+            int nmb = 0;
+
+            try {
+
+            nmb = sc.nextInt();
+            isNumber = true;
+
+            }
+            catch (InputMismatchException n) {
+
+                System.err.println("\nInvalid entry, select the type of item (select by typing one of the numbers next to the options)\n1. Book\n2. Food\n3. Medical\n4. Other\n");
+                sc.nextLine();
+            }
+
+            if (isNumber) {
+                if (nmb == 1) {
+                    type = 1;
+                    break;
+                }
+                else if (nmb == 2) {
+                    type = 2;
+                    break;
+                }
+                else if (nmb == 3) {
+                    type = 3;
+                    break;
+                }
+                else if (nmb == 4) {
+                    type = 4;
+                    break;
+                }
+                else {
+                    System.out.println("\nInvalid entry, select the type of item (select by typing one of the numbers next to the options)\n1. Book\n2. Food\n3. Medical\n4. Other\n");
+                }
+            }
+
+        }
+
+        Item item = new Item(name, price, quantity, imported, type);
+
+        items.add(item);
+
+        System.out.println("\nDo you want to add another item : (Y/N) [any other key except y/Y is considered as N]\n");
+        sc.nextLine();
+
+        String str1 = sc.nextLine();
+
+        if (str1.matches("y") || str1.matches("Y"))
+            itemCount++;
+        else
+            break;
+
+        }
+
+        System.out.println("The number of items you have entered is: " + items.size());
+
     }
 }
---
+++ assignment/src/main/java/com/sales_taxes/model/Item.java
@@ -0,0 +1,69 @@
+package com.sales_taxes.model;
+
+public class Item {
+    
+    private String name;
+    private Double price;
+    private int quantity;
+    private Boolean imported;
+    private int type;
+    private Double afterTax;
+
+    public Item(String name, Double price, int quantity, Boolean imported, int type) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.imported = imported;
+        this.type = type;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Double getPrice() {
+        return this.price;
+    }
+
+    public int getQuantity() {
+        return this.quantity;
+    }
+
+    public Boolean getImported() {
+        return this.imported;
+    }
+
+    public int getType() {
+        return this.type;
+    }
+
+    public Double getAfterTax() {
+        return this.afterTax;
+    }
+
+    public void setAfterTax(Double afterTax) {
+        this.afterTax = afterTax;        
+    }
+
+    public void calculateItemTax() {
+
+        double importTax = 0;
+        double otherTax = 0;
+
+        if (imported) {
+            importTax = price * quantity * 0.05;
+        }
+
+        if (type == 4) {
+            otherTax = price * quantity * 0.1;
+        }
+
+        afterTax = importTax + otherTax;
+
+        afterTax = Math.round(afterTax*100.0)/100.0;
+
+    }
+
+
+    
+}
