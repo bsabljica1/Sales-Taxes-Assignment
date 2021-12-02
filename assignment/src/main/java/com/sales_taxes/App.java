8a20c5a1cbc12667ce99b1b99b42fd1b02227924
bsabljica1 <44245997+bsabljica1@users.noreply.github.com>
2021-12-02 14:03:43 +0100

    Add files via upload
    Initial commit of created application

---
+++ assignment/src/main/java/com/sales_taxes/App.java
@@ -0,0 +1,10 @@
+package com.sales_taxes;
+
+public final class App {
+    private App() {
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
